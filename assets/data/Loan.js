18fcc001aa2a950e95d3a0ae0c1b78ec1b973c3e
Lizhi Lv <tianli052124@gmail.com>
2016-12-06 15:58:48 +0000

    Upload loan data

---
+++ assets/data/Loan.js
@@ -0,0 +1,47 @@
+$scope.category = [
+  {
+    LoanID : 0,
+    Due : "2016-12-15",
+    Datestarted : "2016-09-09",
+    ExtensionRequested: false,
+    Course: "GC02 App Design",
+    Returned: null,
+    OntheFly: null,
+    DamageReported: null,
+    Approved: true,
+    Length: 30 ,
+    deviceID: 1,
+    ClientID: 0,
+    StaffID: 0
+  },
+  {
+    LoanID : 1,
+    Due : null,
+    Datestarted : null,
+    ExtensionRequested: null,
+    Course: "GC02 App Design",
+    Returned: null,
+    OntheFly: null,
+    DamageReported: null,
+    Approved: false,
+    Length: 30 ,
+    deviceID: 0,
+    ClientID: 1,
+    StaffID: 0
+  },
+  {
+    LoanID : 2,
+    Due : "2016-12-15",
+    Datestarted : "2016-09-09",
+    ExtensionRequested: false,
+    Course: "GC02 App Design",
+    Returned: null,
+    OntheFly: null,
+    DamageReported: null,
+    Approved: true,
+    Length: 30 ,
+    deviceID: 1,
+    ClientID: 0,
+    StaffID: 0
+  },
+]
