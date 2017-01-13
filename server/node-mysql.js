50aba3b265980f37238c5194cd195d7875f5a555
Pete <meltzerpete@gmail.com>
2017-01-13 12:38:14 +0000

    fixed names in client search

--- server/node-mysql.js
+++ server/node-mysql.js
@@ -139,7 +139,7 @@ app.delete("/api/clients", function(req, res){
  	if (req.query.clientID) {
  		//client is specified - return single client
 
-    connection.query('SELECT client_id AS clientID, client_firstname AS clientFirstName, client_lastname AS client_clientLastName, ' +
+    connection.query('SELECT client_id AS clientID, client_firstname AS clientFirstName, client_lastname AS clientLastName, ' +
     'client_course AS clientCourse, client_supervisor AS clientSupervisor, client_type AS clientType, client_studentno AS clientStudentNo, client_email AS clientEmail  FROM client WHERE client_id =?',
   [parseInt(req.query.clientID)],function(err,row){
   //  connection.end();
@@ -148,7 +148,7 @@ app.delete("/api/clients", function(req, res){
   });
  	} else {
  		//no client specified - return array of all categories
-    connection.query('SELECT client_id AS clientID, client_firstname AS clientFirstName, client_lastname AS client_clientLastName, ' +
+    connection.query('SELECT client_id AS clientID, client_firstname AS clientFirstName, client_lastname AS clientLastName, ' +
     'client_course AS clientCourse, client_supervisor AS clientSupervisor, client_type AS clientType, client_studentno AS clientStudentNo, client_email AS clientEmail  FROM client',
     function(err,row){
     //  connection.end();
